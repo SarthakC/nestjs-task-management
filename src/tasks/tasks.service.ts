8ba3e2a3c18a0242e5cff8f4b6590d8a2b4343a3
Sarthak Chowdhary <sarthak.chow@gmail.com>
2020-11-29 04:34:11 +0530

    created task module, service, controller

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v1 as uuid } from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks = [...this.tasks, task];
+    return task;
+  }
+}
